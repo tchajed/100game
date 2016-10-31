ddc19405a7b8d25f26bb653a15fdad1954e2035a
Tej Chajed <tchajed@mit.edu>
2016-10-31 17:58:20 -0400

    Add initial Coq formalization

---
+++ coq/problem.v
@@ -0,0 +1,55 @@
+(** * Puzzle: The Sum Game *)
+
+(** Consider the following game: two players take turns adding a 
+    number from 1-10 to a shared sum. The player who makes the 
+    sum 100 wins. Who wins, and how?
+
+    #
+    <a href="coq/problem.v">Download the Coq source!</a>
+    #
+
+*)
+
+Require Import Omega Arith.
+Import PeanoNat.Nat.
+
+(** A "state" is a natural number representing how much
+    remains between the current sum and the total (which
+    is 100 in this case. *)
+Definition state := nat.
+
+(** A valid change in state is one which is between 1 and 10. *)
+Inductive validDiff : nat -> Prop :=
+| validDiff_inbounds : forall n, 1 <= n ->
+                            n <= 10 ->
+                            validDiff n.
+
+(** [action n n'] is true if it is a valid move to
+    change the state from [n] to [n'] *)
+Definition action : state -> state -> Prop :=
+  fun n n' => validDiff (n - n').
+
+(** A definition of what it means for the current player
+    to lose or win from a current state. *)
+Inductive LoseFrom : state -> Prop :=
+| AllWin : forall n,
+    (forall n', action n n' -> WinFrom n') ->
+    LoseFrom n
+with WinFrom : nat -> Prop :=
+     | CanForce : forall n n',
+         action n n' ->
+         LoseFrom n' ->
+         WinFrom n.
+
+(** The total target cumulative sum, which is also the starting
+    state.*)
+Definition total : nat := 100.
+
+(** The formal problem statement: Does the first player to play
+    win or lose? *)
+Definition Puzzle : Type := WinFrom total + LoseFrom total.
+
+Theorem PuzzleSolution : Puzzle.
+Proof.
+unfold Puzzle.
+Abort.
---
+++ coq/solution.v
@@ -0,0 +1,162 @@
+Require Import 
+  Arith
+  Omega
+  problem.
+Import PeanoNat.Nat.
+
+
+Definition pre_strategy :=
+  state -> {m | m < 10}.
+
+Definition strategy (s: pre_strategy) :
+  forall n : state, {m | action (S n) m}.
+Proof.
+  intros.
+  destruct (s (S n)) as [m ?].
+  exists ((S n) - (1 + m)).
+  unfold action; constructor; omega.
+Qed.
+
+Definition winning_pre_strategy : pre_strategy.
+Proof.
+  intro n.
+  exists (n mod 11 - 1).
+  pose proof (Nat.mod_upper_bound n 11); omega.
+Defined.
+
+Definition winning_strategy := strategy winning_pre_strategy.
+
+Lemma mod_div_0 : forall n k,
+    k <> 0 ->
+    n mod k / k = 0.
+Proof.
+  intros.
+  pose proof (mod_upper_bound n k); intuition.
+  apply div_small; auto.
+Qed.
+
+Theorem mod_principle k (Heq0: k <> 0) :
+  forall (P: nat -> Prop)
+    (Hsmall: forall n, n < k -> P n)
+    (Hind: forall n, P n -> P (k + n)),
+    (forall n, P n).
+Proof.
+  intros.
+  pose proof (div_mod n k ltac:(auto)).
+  remember (n / k) as p. remember (n mod k) as r.
+  rewrite H.
+  pose proof (mod_upper_bound n k ltac:(assumption)).
+  rewrite <- Heqr in H0.
+  clear n H Heqp Heqr.
+  induction p.
+  - rewrite mul_0_r in *; simpl in *.
+    auto.
+  - rewrite mul_comm; simpl.
+    rewrite <- plus_assoc.
+    apply Hind.
+    rewrite mul_comm. assumption.
+Qed.
+
+Theorem mod_principle' k (Heq0: k <> 0) :
+  forall (P: nat -> Prop)
+    (Hsmall: forall n, n < k -> P n)
+    (Hind: forall p, (forall n, n < k -> P (k *   p + n)) ->
+                (forall n, n < k -> P (k * S p + n))),
+    (forall n, P n).
+Proof.
+  intros.
+  pose proof (div_mod n k ltac:(auto)).
+  remember (n / k) as p. remember (n mod k) as r.
+  rewrite H.
+  pose proof (mod_upper_bound n k ltac:(assumption)).
+  rewrite <- Heqr in H0.
+  clear n H Heqp Heqr.
+  generalize dependent r.
+  induction p.
+  - rewrite mul_0_r in *; simpl in *.
+    auto.
+  - apply Hind. apply IHp.
+Qed.
+
+Inductive action' (n : state) : state -> Prop :=
+  | mkaction : forall k, k < 10 -> action' n (n + S k).
+
+Lemma action_action' : forall n m,
+  action n m <-> action' m n.
+Proof.
+intros. split; intros.
+- inversion H. clear H. subst.
+  rewrite (le_plus_minus m n).
+  pose proof (mkaction m (pred (n - m))).
+  assert (n - m <> 0).
+  unfold not. intros. omega.
+  rewrite succ_pred in H by assumption. apply H.
+  unfold "<". rewrite succ_pred by assumption.
+  assumption. apply lt_le_weak.
+  apply lt_O_minus_lt. assumption.
+- induction H. econstructor; omega.
+Qed.
+
+Lemma LoseFrom0 : LoseFrom 0.
+Proof.
+econstructor.
+intros. rewrite action_action' in H. 
+inversion H. omega.
+Qed.
+
+Lemma WinFrom_n : forall n k x, x = n + S k -> LoseFrom n -> k < 10 -> WinFrom x.
+Proof.
+intros. subst.
+econstructor. 2:eassumption.
+apply action_action'.
+constructor. assumption.
+Qed.
+
+Lemma LoseFrom_n : forall n, LoseFrom n -> LoseFrom (11 + n).
+Proof.
+intros.
+constructor.
+intros. 
+inversion H0; subst.      
+eapply WinFrom_n.
+2: eauto.
+instantiate (1 := n' - n - 1).
+omega.
+omega.
+Qed.
+
+Theorem solution_all : forall n,
+    match n mod 11 with
+    | 0 => LoseFrom n
+    | S _ => WinFrom n
+    end.
+Proof.
+  intros.
+  induction n using (mod_principle' 11 ltac:(omega)).
+  - rewrite mod_small by auto.
+    destruct n.
+    + apply LoseFrom0.
+    + eapply WinFrom_n with 0 n. reflexivity. 
+      apply LoseFrom0. omega. 
+  - assert (forall p x, x < 11 -> (11 * p + x) mod 11 = x).
+    intros. rewrite (plus_comm _ x).
+    rewrite (mul_comm 11). 
+    rewrite mod_add by omega.
+    rewrite mod_small by assumption. reflexivity.
+    rewrite H1 by assumption. destruct n2.
+    + specialize (H 0 ltac:(omega)).
+      rewrite H1 in H by omega.
+      rewrite plus_0_r in *.
+      rewrite mul_comm. rewrite mul_comm in H. apply LoseFrom_n. apply H. 
+    + specialize (H 0 ltac:(omega)).
+      rewrite H1 in H by omega.
+      rewrite plus_0_r in H.
+      apply LoseFrom_n in H.
+      eapply WinFrom_n. 2:eassumption.
+      instantiate (1 := n2). omega. omega.
+Qed.
+
+Theorem solution : WinFrom 100.
+Proof.
+apply (solution_all 100).
+Qed.
